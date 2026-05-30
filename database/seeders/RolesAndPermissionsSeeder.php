01218deca821d5f7192cb12323a0deb4d54c6484
Jesus Morales <gsus@Jesuss-MacBook-Pro.local>
2026-05-30 07:24:43 -0600

    fix:permisos

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -37,18 +37,18 @@ class RolesAndPermissionsSeeder extends Seeder
 
         // Crear rol cajero (acceso solo a transacciones, sin permisos especiales)
         $cajero = Role::firstOrCreate(['name' => 'cajero']);
-        $cajero->syncPermissions(['cuentas.search']);
+        $cajero->syncPermissions($permissions);
 
         // Crear rol servicio_al_cliente (acceso a clientes, cuentas y tickets)
         $servicioAlCliente = Role::firstOrCreate(['name' => 'servicio_al_cliente']);
-        $servicioAlCliente->syncPermissions(['cuentas.search']);
+        $servicioAlCliente->syncPermissions($permissions);
 
         // Crear rol gerente (acceso a todo menos administración)
         $gerente = Role::firstOrCreate(['name' => 'gerente']);
-        $gerente->syncPermissions(['cuentas.search']);
+        $gerente->syncPermissions($permissions);
 
         // Crear rol banco con permiso para registrar transferencias externas
         $banco = Role::firstOrCreate(['name' => 'banco']);
-        $banco->syncPermissions(['transferencias-externas.store', 'cuentas.search']);
+        $banco->syncPermissions($permissions);
     }
 }
